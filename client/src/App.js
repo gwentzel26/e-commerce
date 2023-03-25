0bddfd38432854ba107eae7d5b3eb1ecd69bff33
Gavin Wentzel <gavinwentzel@gmail.com>
2023-03-25 14:54:48 -0400

    Completed Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import Home from "./scenes/home/Home"
+import Checkout from './scenes/checkout/checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/navbar';
+import Home from "./scenes/home/Home";
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+
 
 const ScrollToTop = () => {
   const { pathname} = useLocation();
@@ -16,9 +21,13 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+      <Navbar />
         <ScrollToTop>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='item/:itemId' element={<ItemDetails />} />
+            <Route path='checkout' element={<Checkout />} />
+            <Route path='checkout/success' element={<Confirmation />} />
 
            
           </Routes>
