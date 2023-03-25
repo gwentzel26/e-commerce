7838f5cbbcf2683f2225556deb7f89baba527b5a
Gavin Wentzel <gavinwentzel@gmail.com>
2023-03-25 16:15:30 -0400

    Create initialState and addToCart functions in index.js

---
+++ client/src/scenes/state/index.js
@@ -0,0 +1,36 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    isCartOpen: false,
+    cart: [],
+    items: [],
+}
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        setItems:(state, action) => {
+            state.items = action.payload;
+        },
+
+        addToCart: (state, action) => {
+            state.cart = [...state.cart, action.payload.item];
+        },
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+        },
+        increaseCount: (state, action) => {
+            state.cart = state.cart.map((item) => {
+                if(item.id === action.payload.id) {
+                    item.count++;
+                }
+                
+            })
+        }
+    }
+})
+
+export const {setItems} = cartSlice.actions;
+
+export default cartSlice.reducer;
