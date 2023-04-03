7a81bb999359bffb843d1b1d79e7ad8de9a4c85c
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-03 11:19:32 -0400

    Started creating the cart menu

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { CssBaseline } from '@mui/material';
 import { theme } from './theme';
 import { Provider } from "react-redux";
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from "./state";
+import cartReducer from "./scenes/state";
 
 const store = configureStore({
   reducer: { cart: cartReducer },
