14394341735aec87fbd3ce56021477cd38d8d40b
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-03 12:08:04 -0400

    Completed Modal, header, and Cart list

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -5,8 +5,9 @@ import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import styled from "@emotion/styled";
 import shades from "../../theme";
-import { decreaseCount, increaseCount, setIsCartOpen, removeFromCart  } from "../state";
+import state, { decreaseCount, increaseCount, setIsCartOpen, removeFromCart  } from "../state";
 import { useNavigate } from "react-router-dom";
+import ItemDetails from "../itemDetails/ItemDetails";
 
 const FlexBox = styled(Box)`
     display: flex;
@@ -18,7 +19,83 @@ const CartMenu = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const cart = useSelector((state) => state.cart.cart);
-    const 
+    const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+    const totalPrice = cart.reduce((total, item) => {
+        return total + item.count * item.attributes.price;
+    }, 0);
 }
 
+return (
+    <Box // overlay
+        display={isCartOpen ? "block" : 'none'}
+        backgroundColor="rgba(0, 0, 0, 0.4)"
+        position="fixed"
+        zIndex={10}
+        width="100%"
+        height="100%"
+        left="0" 
+        top="0"
+        overflow="auto"
+        >
+            {/* {MODAL} */}
+            <Box
+                position= "fixed"
+                right="0"
+                bottom="0"
+                width="max(400px, 30%)"
+                height="100%"
+                backgroundColor="white"
+                >
+                    <Box padding="30px" overflow="auto" height ="100%">
+                        {/* {HEADER} */}
+                        <FlexBox mb="15px">
+                            <Typography variant="h3">SHOPPING BAG ({cart.length})</Typography>
+                            <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+                                <CloseIcon />
+                            </IconButton>
+                        </FlexBox>
+                    </Box>
+
+                    {/* {CART LIST} */}
+                    <Box>
+                        {cart.map((item) => (
+                            <Box key={`${item.attributes.name}-${item.id}`}> 
+                                <FlexBox p="15px 0">
+                                    <Box flex="1 1 40%">
+                                      <img
+                                        alt={item?.name}
+                                        width="123px"
+                                        height="164px"
+                                        src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}` }
+                                        />
+                                    </Box>
+                                    <Box flex="1 1 60%">
+                                        <FlexBox mb="5px">
+                                            <Typography fontWeight="bold">
+                                                {item.attributes.name}
+                                            </Typography>
+                                            <IconButton onClick={()=> dispatch(removeFromCart({ id: item.id}))}>
+                                                <CloseIcon />
+                                            </IconButton>
+                                        </FlexBox>
+                                        <Typography>{item.attributes.shortDescription}</Typography>
+                                        <FlexBox m="15px 0">
+                                            <Box
+                                            display="flex"
+                                            alignItems="center"
+                                            border={`1.5px solid ${shades.neutral[500]}`}>
+                                                
+                                            </Box>
+                                        </FlexBox>
+                                    </Box>
+                                </FlexBox>
+                            </ Box>
+                        )
+
+                        )}
+                    </Box>
+            </Box>
+
+        </Box>
+)
 export default CartMenu;
