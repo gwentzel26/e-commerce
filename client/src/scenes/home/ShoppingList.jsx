b0cbbddcba86c6c73b410731e252dcadc602371c
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-10 13:13:06 -0400

    Shopping List

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -4,3 +4,33 @@ import { Box, Typography, Tab, Tabs } from "@mui/material";
 import Item from "../../components/Item";
 import { setItems } from '../../state';
 
+const ShoppingList = () => {
+    const dispatch = useDispatch();
+    const [value, setValue] = useState("all");
+    const items = useSelector((state) => state.cart.items);
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+    console.log("items", items);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    async function getItems() {
+        const items = await fetch(
+            "http://localhost:1337/api/items?populate=image",
+            {method: "GET"}
+        );
+    
+    const itemsJson = await items.json();
+    dispatch(setItems(itemsJson.data));
+        }
+
+    useEffect(() => {
+        getItems();
+
+    }, [])
+
+    return (
+        <div>Shopping List</div>
+    )
+};
