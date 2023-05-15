369bde653bfd9883938f8a0c038657bed808e736
Gavin Wentzel <gavinwentzel@gmail.com>
2023-05-15 13:54:40 -0400

    Added footer

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,6 +1,7 @@
 import MainCarousel from "./MainCarousel";
 import ShoppingList from "./ShoppingList";
 import Subscribe from "./Subscribe";
+import Footer from "../global/Footer";
 import "../../css/Home.css";
 
 
@@ -10,6 +11,7 @@ const Home = () => {
         <MainCarousel />
         <ShoppingList />
         <Subscribe />
+        <Footer />
     </div>;
 };
 
